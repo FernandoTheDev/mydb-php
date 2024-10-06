1391e408a76c69f1c8828a0b720340ccbe9b8bc0
Fernando <fernandolovephp@gmail.com>
2024-10-06 20:23:23 -0300

    Testando

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fernandothedev/mydb-php',
+        'pretty_version' => '0.1.0',
+        'version' => '0.1.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fernandothedev/mydb-php' => array(
+            'pretty_version' => '0.1.0',
+            'version' => '0.1.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jakub-onderka/php-console-color' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'php-parallel-lint/php-console-color' => array(
+            'pretty_version' => 'v1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '7adfefd530aa2d7570ba87100a99e2483a543b88',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-parallel-lint/php-console-color',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
